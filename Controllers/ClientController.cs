739ff46a645624a2408dca5b9579eeedfddb82ab
Sebatora <sebastiana.toranzo@gmail.com>
2023-11-10 12:15:28 -0300

    first commit

---
+++ Controllers/ClientController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetCore_Web_Api.Models;
+
+namespace NetCore_Web_Api.Controllers
+{
+  [ApiController]
+  [Route("client")]
+  public class ClientController : ControllerBase
+  {
+    [HttpGet]
+    [Route("list")]
+    public dynamic listClient()
+    {
+      List<Client> clients = new List<Client>
+      {
+        new Client
+        {
+          id = "1",
+          name = "Juan",
+          age = "33",
+          mail = "juan@mail.com"
+        },
+        new Client
+        {
+          id = "2",
+          name = "Rodrigo",
+          age = "23",
+          mail = "rodrigo@mail.com"
+        }
+      };
+
+      return clients;
+    }
+
+/*    [HttpPost]
+    [Route("save")]
+    public dynamic saveClient()
+    {
+
+    }*/
+
+  }
+}
---
+++ Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace NetCore_Web_Api.Models
+{
+  public class Client
+  {
+    public string id { get; set; }
+    public string name { get; set; }
+    public string age { get; set; }
+    public string mail { get; set; }
+
+  }
+}
